affe8df966ef6a9ffb2e10ea892508c98771fc8d
Laura Green <laura.eliza.green@gmail.com>
2024-01-04 10:55:55 -0700

    separate code pieces to different files

---
+++ Piano_Lyrics_OLED/src/Piano_Lyrics_OLED.cpp
@@ -0,0 +1,57 @@
+/* 
+ * Project Piano_Lyrics_OLED
+ * Author: Laura Green
+ * Date: 1/4/24
+ * For comprehensive documentation and examples, please visit:
+ * https://docs.particle.io/firmware/best-practices/firmware-template/
+ */
+
+// Include Particle Device OS APIs
+#include "Particle.h"
+#include "IoTClassroom_CNM.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1306.h"//specific OLED device
+
+
+
+// Let Device OS manage the connection to the Particle Cloud
+SYSTEM_MODE(AUTOMATIC);
+
+// Run the application and system concurrently in separate threads
+SYSTEM_THREAD(ENABLED);
+
+const int OLED_RESET=-1;
+const int NUMFLAKES=10;//like const (datatype=int?) NUMFLAKES=10; what is this constant?
+const int XPOS=0; 
+const int  YPOS=1;
+const int DELTAY=2;
+
+Adafruit_SSD1306 display(OLED_RESET);
+
+// Show system, cloud connectivity, and application logs over USB
+// View logs with CLI using 'particle serial monitor --follow'
+//SerialLogHandler logHandler(LOG_LEVEL_INFO);
+
+// setup() runs once, when the device is first turned on
+void setup() {
+  Serial.begin(9600); 
+  waitFor(Serial.isConnected,15000);
+
+display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //initialize with 12C address----not sure I understand this....??
+display.clearDisplay();   // clears the screen and buffer
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  display.setTextSize(1);//code to add my name and birthdate
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.printf(threeBlindMice[i]\n); //song lyrics here "three blind mice"? make into array and loop through with each note
+  display.display();
+  delay(1000); 
+
+  // Example: Publish event to cloud every 10 seconds. Uncomment the next 3 lines to try it!
+  // Log.info("Sending Hello World to the cloud!");
+  // Particle.publish("Hello world!");
+  // delay( 10 * 1000 ); // milliseconds and blocking - see docs for more info!
+}
---
+++ Piano_photodiode_code/src/Piano_photodiode_code.cpp
@@ -0,0 +1,96 @@
+/* 
+ * Project Piano_photodiode
+ * Author: Laura Green
+ * Date: 1/4/24
+ * For comprehensive documentation and examples, please visit:
+ * https://docs.particle.io/firmware/best-practices/firmware-template/
+ */
+
+// Include Particle Device OS APIs
+#include "Particle.h"
+#include "IoTClassroom_CNM.h"
+#include <neopixel.h>
+
+
+
+
+
+SYSTEM_MODE(SEMI_AUTOMATIC);
+//SYSTEM_MODE(AUTOMATIC);//for room control
+
+
+SYSTEM_THREAD(ENABLED);
+const int BULB=2;
+const int PHOTODIODE=A1;
+const int SERVPIN=A2;//has to be PWM
+const int BUZZPIN= A5;//PWM?
+const int NOTELEVEL=100;//how much light photodiode has to have to trigger hue light
+const int NOTEDELAY=2000;//will need to be varied, so maybe later not constant? how long light stays on
+const int DURATION=1000;//also may need to be varied-how long note plays, same as delay, so could be same variable
+const int PIXELCOUNT=1;//use one neopixel to check hue light when not at FUSE
+const int CNOTE=131;//freq for C notes could be h file later? or use pitch.h
+const int DNOTE=147;//freq for D
+const int ENOTE=165;//freq for E
+const int FNOTE=175;//freq for F
+const int GNOTE=196;//freq for G
+const int ANOTE=220;//freq for A
+const int BNOTE=247;//freq for B
+const int HICNOTE=262;//freq for high C
+
+int lightLevelC;
+int color;
+unsigned int timerStart, currentTime;
+
+Servo cServo;//create object ...servo class built into photon library
+IoTTimer btwNoteTimer;
+Adafruit_NeoPixel pixel(PIXELCOUNT, SPI1, WS2812B);
+
+
+void setup() {
+  Serial.begin(9600); 
+  waitFor(Serial.isConnected,15000);
+
+  /*
+  WiFi.on();  //comment out this section when not at FUSE
+  WiFi.clearCredentials();
+  WiFi.setCredentials("IoTNetwork");
+
+  WiFi.connect();
+  while(WiFi.connecting()) {
+    Serial.printf(".");
+  }  
+  Serial.printf("\n\n");
+  */
+
+  pinMode(PHOTODIODE,INPUT);//photodiode
+  pinMode(BUZZPIN,OUTPUT);//buzzer
+  cServo.attach(SERVPIN);//motor for C key (or all keys?)
+
+pixel.begin();//to initialize neopixel for home tests
+pixel.setBrightness (20); 
+pixel.show();
+btwNoteTimer.startTimer(100);
+
+
+  
+}
+
+
+void loop() {
+  if(lightLevelC>NOTELEVEL){
+    //setHue(BULB,true,HueViolet,50,255);
+    cServo.write(90);//0-200 degrees  maybe will need supplemental power and how long will it take?
+    //noteTimer.isTimerReady();
+    tone(BUZZPIN,CNOTE,DURATION);
+    //delay(DURATION+1000);
+  }
+
+  //btwNoteTimer.startTimer(NOTEDELAY); //how long light stays on/in between notes if looped
+    
+  //delay(1000);
+   //setHue(BULB,false,50,255); //how to turn off btw and timing (set timer with variable as target?  )
+    //delay(5000);
+ 
+
+  
+}
