70da2e2b25fd4a10fc729decd107434efdf88ccb
Laura Green <laura.eliza.green@gmail.com>
2024-01-05 16:45:59 -0700

    still trying mp3

--- Piano_keys_mp3/src/Piano_keys_mp3.cpp
+++ Piano_keys_mp3/src/Piano_keys_mp3.cpp
@@ -49,26 +49,36 @@ void setup() {
   }
   Serial.println(F("DFPlayer Mini online."));
 
-  myDFPlayer.volume(10);  //Set volume value. From 0 to 30
+  myDFPlayer.volume(20);  //Set volume value. From 0 to 30
   myDFPlayer.play(1);  //Play the first mp3
+  //delay(2000);
 
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
+//static unsigned long timer = millis();
+
+ // if (millis() - timer > 3000) {
+   // timer = millis();
+    //myDFPlayer.next();  //Play next mp3 every 3 second.
+  }
   
   //piano1 1st folder, note tracks in alpha order
-  myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
-  myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
+  //myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
+  //myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
   //myDFPlayer.pause();
   //pausePlay=0;
-  myDFPlayer.volume(10);
+  //myDFPlayer.volume(10);
 
 //myDFPlayer.play(1);  //Play the first mp3
   //delay(1000); 
 //myDFPlayer.randomAll();
-myDFPlayer.playFolder(1,1);  //play specific mp3 in SD:/15/004.mp3; Folder Name(1~99); File Name(1~255)
-
+//myDFPlayer.playFolder(1,1);  //play specific mp3 in SD:/15/004.mp3; Folder Name(1~99); File Name(1~255)
+/*
+if (myDFPlayer.available()) {
+    printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
+*/
  //to play mp3 tracks when button pressed 
  /*   
 if(cButton.isPressed()) {
@@ -86,4 +96,67 @@ if(dButton.isPressed()) {
   // Log.info("Sending Hello World to the cloud!");
   // Particle.publish("Hello world!");
   // delay( 10 * 1000 ); // milliseconds and blocking - see docs for more info!
+
+/*
+void printDetail(uint8_t type, int value){
+  switch (type) {
+    case TimeOut:
+      Serial.println(F("Time Out!"));
+      break;
+    case WrongStack:
+      Serial.println(F("Stack Wrong!"));
+      break;
+    case DFPlayerCardInserted:
+      Serial.println(F("Card Inserted!"));
+      break;
+    case DFPlayerCardRemoved:
+      Serial.println(F("Card Removed!"));
+      break;
+    case DFPlayerCardOnline:
+      Serial.println(F("Card Online!"));
+      break;
+    case DFPlayerUSBInserted:
+      Serial.println("USB Inserted!");
+      break;
+    case DFPlayerUSBRemoved:
+      Serial.println("USB Removed!");
+      break;
+    case DFPlayerPlayFinished:
+      Serial.print(F("Number:"));
+      Serial.print(value);
+      Serial.println(F(" Play Finished!"));
+      break;
+    case DFPlayerError:
+      Serial.print(F("DFPlayerError:"));
+      switch (value) {
+        case Busy:
+          Serial.println(F("Card not found"));
+          break;
+        case Sleeping:
+          Serial.println(F("Sleeping"));
+          break;
+        case SerialWrongStack:
+          Serial.println(F("Get Wrong Stack"));
+          break;
+        case CheckSumNotMatch:
+          Serial.println(F("Check Sum Not Match"));
+          break;
+        case FileIndexOut:
+          Serial.println(F("File Index Out of Bound"));
+          break;
+        case FileMismatch:
+          Serial.println(F("Cannot Find File"));
+          break;
+        case Advertise:
+          Serial.println(F("In Advertise"));
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+
 }
+*/
