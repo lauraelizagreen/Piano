7464f4eac8bdeedf893d0fcf6c152aea1b08ee33
Laura Green <laura.eliza.green@gmail.com>
2024-01-10 02:32:01 -0700

    end of day

--- Piano_photodiode_code/src/Piano_photodiode_code.cpp
+++ Piano_photodiode_code/src/Piano_photodiode_code.cpp
@@ -21,7 +21,8 @@ SYSTEM_MODE(SEMI_AUTOMATIC);
 
 SYSTEM_THREAD(ENABLED);
 //declare component pins
-const int WEMO1=1;
+const int WEMO1=1;//spotlight
+const int WEMO2=5;//fan
 const int BULB=2;//hue bulb at my desk
 const int PHOTODIODEONE=A0;
 const int PHOTODIODETWO=A1;
@@ -34,8 +35,8 @@ const int NOTELEVEL=50;//how much light photodiode has to have to trigger hue li
 const int NOTEDELAY=2000;//will need to be varied, so maybe later not constant? how long light stays on
 const int DURATION=1000;//also may need to be varied-how long note plays, same as delay, so could be same variable
 const int PIXELCOUNT=1;//use one neopixel to check hue light when not at FUSE and neopixel for on/off indicator light
-//declare all note frequencies
-const int CNOTE=131;//freq for C notes could be h file later? or use pitch.h
+//declare all note frequencies: notes could be h file later? or use pitch.h
+const int CNOTE=131;//freq for C 
 const int DNOTE=147;//freq for D
 const int ENOTE=165;//freq for E
 const int FNOTE=175;//freq for F
@@ -45,7 +46,7 @@ const int BNOTE=247;//freq for B
 const int HICNOTE=262;//freq for high C
 //declare all button pins
 const int HICBUTTON=D17;
-const int BBUTTON=D2;
+const int BBUTTON=D15;//D2
 const int ABUTTON=D3;
 const int GBUTTON=D4;
 const int FBUTTON=D5;
@@ -53,7 +54,7 @@ const int EBUTTON=D6;
 const int DBUTTON=D7;
 const int CBUTTON=D10;
 const int ENCSWITCH=D19;
-const int GREENLED=D18;
+const int GREENLED=D18;//light on encoder
 //const int REDLED=D16;//not enough pins?
 
 int lightLevelOne;//photodiode 1-4
@@ -69,14 +70,14 @@ bool onOff;
 Servo cServo;//create object ...servo class built into photon library
 Adafruit_NeoPixel pixel(PIXELCOUNT, SPI1, WS2812B);
 Button hicButton(HICBUTTON);
-Button bButton(BBUTTON);
+Button bButton(BBUTTON);  
 Button aButton(ABUTTON);
 Button gButton(GBUTTON);
 Button fButton(FBUTTON);
 Button eButton(EBUTTON);
 Button dButton(DBUTTON);
 Button cButton(CBUTTON);
-Button encSwitch(ENCSWITCH,FALSE);
+Button encSwitch(ENCSWITCH);//false for internal pull-down (not pull-up)
 IoTTimer noteTimer;
 IoTTimer btwNoteTimer;
 
@@ -85,7 +86,7 @@ void setup() {
   Serial.begin(9600); //serial port
   waitFor(Serial.isConnected,15000);
 
-  /*
+  
   WiFi.on();  //comment out this section when not at FUSE
   WiFi.clearCredentials();
   WiFi.setCredentials("IoTNetwork");
@@ -95,7 +96,9 @@ void setup() {
     Serial.printf(".");
   }  
   Serial.printf("\n\n");
-  */
+
+  //pinMode(BBUTTON,INPUT);//to test if still voltage at pin D2
+  
 
   pinMode(PHOTODIODEONE,INPUT);//photodiode 1
   pinMode(PHOTODIODETWO,INPUT);//photodiode 2
@@ -104,16 +107,19 @@ void setup() {
 
   pinMode(GREENLED, OUTPUT);//light on encoder switch
 
-  pinMode(BUZZPIN,OUTPUT);//buzzer
+  //pinMode(BUZZPIN,OUTPUT);//buzzer
   cServo.attach(SERVPIN);//motor for C key (or all keys?)
   onOff=true;//for encoder switch-start true since on is off (ground completes circuit)
 
 pixel.begin();//to initialize neopixel for home tests
 pixel.setBrightness (20); 
 pixel.show();
-btwNoteTimer.startTimer(100);
+//btwNoteTimer.startTimer(100);
 
-//tone(BUZZPIN,FNOTE);
+//tone(BUZZPIN,FNOTE);//just to test buzzer
+digitalWrite(GREENLED,onOff);//not sure if I need this
+wemoWrite(WEMO1,onOff);
+
 
 
   
@@ -127,8 +133,8 @@ void loop() {
 
   }
   digitalWrite(GREENLED,onOff);//low turns on (connects to ground to complete circuit)
-  wemoWrite(WEMO1,onOff);//still add turning switch red/green?
-
+  wemoWrite(WEMO2,onOff);//still add turning switch red/green? spotlight on
+Serial.printf("onOff=%b\n",onOff);
   
   //////FOR AUTO MODE WITH SCROLL
   //setHue(BULB,false,50,255); //light off
@@ -136,35 +142,51 @@ void loop() {
   lightLevelTwo=analogRead(PHOTODIODETWO);
   lightLevelThree=analogRead(PHOTODIODETHREE);
   lightLevelFour=analogRead(PHOTODIODEFOUR);
-  Serial.printf("Light level one is %i\n",lightLevelOne); //what light level is usual? use this to de-bug etc
-  Serial.printf("Light level two is %i\n",lightLevelTwo);
-  Serial.printf("Light level three is %i\n",lightLevelThree);
-  Serial.printf("Light level four is %i\n",lightLevelFour);
+  //Serial.printf("Light level one is %i\n",lightLevelOne); //what light level is usual? use this to de-bug etc
+  //Serial.printf("Light level two is %i\n",lightLevelTwo);
+  //Serial.printf("Light level three is %i\n",lightLevelThree);
+  //Serial.printf("Light level four is %i\n",lightLevelFour);
   /*
   if(lightLevelOne>NOTELEVEL){
-    //setHue(BULB,true,HueViolet,50,255);
+    setHue(BULB,true,HueYellow,50,255);
     //cServo.write(90);//0-200 degrees  maybe will need supplemental power and how long will it take? maybe set as sin wave for piano key movement
     //noteTimer.isTimerReady();
     tone(BUZZPIN,CNOTE);//timing? just as long as exposed to light?
     //delay(DURATION+1000);//may not need
   }
+  setHue(BULB,false);
+  /*
   if(lightLevelTwo>NOTELEVEL) {
     tone(BUZZPIN,DNOTE);
   }
   if(lightLevelThree>NOTELEVEL) {
     tone(BUZZPIN,ENOTE);
   }
-  */
+  
   if(lightLevelFour>NOTELEVEL) {
     tone(BUZZPIN,FNOTE);
   }
-  /////FOR MANUAL MODE PRESSING PIANO KEYS
-if(hicButton.isPressed()) {
-  tone(BUZZPIN,HICNOTE);//stays on while pressed
+  if(lightLevelOne>NOTELEVEL)&&(lightLevelTwo>NOTELEVEL)&&(lightLevelThree>NOTELEVEL)&&(lightLevelFour>NOTELEVEL) {
+    wemoWrite(WEMO1,TRUE);
   }
-if(bButton.isPressed())  {//with pull-down resistor 10kohms >2vreads high  <0.8 reads low  avoid in-btw
+  */
+  /////FOR MANUAL MODE PRESSING PIANO KEYS
+if(hicButton.isPressed()) {//if it were "while" would not move to other parts of code
+  ///tone(BUZZPIN,HICNOTE);//stays on while pressed
+  //Serial.printf("button state is" %)
+  }
+  else{//without else -just noTone can look like working bc looping so fast
+  //noTone(BUZZPIN);
+  }
+  
+//if(bButton.isPressed())  {//with pull-down resistor 10kohms >2vreads high  <0.8 reads low  avoid in-btw
   tone(BUZZPIN,BNOTE);
   }
+  //else{
+   // noTone(BUZZPIN);
+  //}
+
+/*
 if(aButton.isPressed())  {
   tone(BUZZPIN,ANOTE);
   }
@@ -186,10 +208,11 @@ if(cButton.isPressed())  {
 
 //noteTimer.isTimerReady() 
 noTone(BUZZPIN);
+*/
 
 
  
-}
+
 
   //btwNoteTimer.startTimer(NOTEDELAY); //how long light stays on/in between notes if looped
     
