3949663a513e3fb0c162c4a2d051beddc38e70fb
Laura Green <laura.eliza.green@gmail.com>
2024-01-13 13:27:03 -0700

    work on OLED code-not added to main yet, only communicating part of the time

--- Piano_Lyrics_OLED/src/Piano_Lyrics_OLED.cpp
+++ Piano_Lyrics_OLED/src/Piano_Lyrics_OLED.cpp
@@ -13,44 +13,44 @@
 #include "Adafruit_SSD1306.h"//specific OLED device
 
 
-
-// Let Device OS manage the connection to the Particle Cloud
-SYSTEM_MODE(SEMI_AUTOMATIC);
-
-// Run the application and system concurrently in separate threads
-SYSTEM_THREAD(ENABLED);
-
 const int OLED_RESET=-1;
+Adafruit_SSD1306 display(OLED_RESET);
+
 const int NUMFLAKES=10;//like const (datatype=int?) NUMFLAKES=10; what is this constant?
 const int XPOS=0; 
 const int  YPOS=1;
 const int DELTAY=2;
-char threeBlindMice[]={'THREE','BLIND','MICE'};//declare lyrics as array
-int i;//looping through lyrics how many times tones
+//char threeBlindMice[]={'THREE','BLIND','MICE'};//declare lyrics as array
+//int i;//looping through lyrics how many times tones
+SYSTEM_MODE(SEMI_AUTOMATIC);
+SYSTEM_THREAD(ENABLED);
 
-Adafruit_SSD1306 display(OLED_RESET);
 
-// Show system, cloud connectivity, and application logs over USB
-// View logs with CLI using 'particle serial monitor --follow'
-//SerialLogHandler logHandler(LOG_LEVEL_INFO);
-
-// setup() runs once, when the device is first turned on
 void setup() {
   Serial.begin(9600); 
-  waitFor(Serial.isConnected,15000);
+  waitFor(Serial.isConnected,10000);
 
 display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //initialize with 12C address----not sure I understand this....??
+void setRotation(uint8_t rotation);//how to use this to flip?
 display.clearDisplay();   // clears the screen and buffer
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
-  display.setTextSize(1);//code to add my name and birthdate
+  display.clearDisplay();
+  display.setTextSize(3);//
   display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.printf("%c\n",threeBlindMice[0]); //song lyrics here "three blind mice"? make into array and loop through with each note
+  display.setCursor(0,5);
+  //display.printf("%c\n",threeBlindMice[0]); //song lyrics here "three blind mice"? make into array and loop through with each note
+  display.printf("MANUAL MODE");
+  //display.startscrollright(0x00, 0x0F);
   display.display();
-  delay(1000); 
+  //delay(5000); 
+  //display.clearDisplay();
+  //display.setCursor(3,0);
+  //display.printf("AUTOMATIC");
+  //display.display();
+  //delay(1000);
 
   // Example: Publish event to cloud every 10 seconds. Uncomment the next 3 lines to try it!
   // Log.info("Sending Hello World to the cloud!");
